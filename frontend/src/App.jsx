17d67cca61763eee8b4496def938cbf955bb352c
zayqu <mnyamwezime@gmail.com>
2025-10-27 22:50:28 +0300

    Implement Protected Routes and update App structure for role-based access control

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,31 @@
-import './App.css'
-import  {BrowserRouter as Router, Route, Routes} from 'react-router'
-import Root from './components/Root'
-import Login from './pages/login'
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router';
+import Root from '../utils/Root';
+import Login from './pages/login';
+import ProtectedRoutes from '../utils/ProtectedRoutes.jsx';
 
 function App() {
-
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Root />} />
-        <Route path="/admin/dashboard" element={<h1>Admin Dashboard</h1>} />
+        <Route
+          path="/admin/dashboard"
+          element={
+            <ProtectedRoutes requireRole={["admin"]}>
+              <h1>Admin Dashboard</h1>
+            </ProtectedRoutes>
+          }
+        />
         <Route path="/customer/dashboard" element={<h1>Customer Dashboard</h1>} />
         <Route path="/login" element={<Login />} />
+        <Route
+          path="/unauthorized"
+          element={<p className="font-bold text-3xl mt-20 ml-20">Unauthorized Access</p>}
+        />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
